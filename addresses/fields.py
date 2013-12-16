4009d4fd0cd7b7ad5ea046ceac1199bde4163308
Jiri Barton <jbar@hosting4u.cz>
2013-12-16 11:45:56 +0100

    passwords

--- addresses/fields.py
+++ addresses/fields.py
@@ -1,8 +1,11 @@
+import random
+import crypt
 from django.db import models
 
 
+CHARSET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789./'
 def encode_password(plain):
-    return plain #TODO
+    return crypt.crypt('plain', '$6$%s$' % ''.join(random.choice(CHARSET) for _ in xrange(16)))
 
 class PasswordCharField(models.CharField):
     def save_form_data(self, instance, data):
