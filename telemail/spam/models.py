ca717b9427aab6c3608beb2b87b6c737a5a30821
Jiri Barton <jbar@hosting4u.cz>
2013-12-17 22:58:24 +0100

    explanations

--- telemail/spam/models.py
+++ telemail/spam/models.py
@@ -2,30 +2,30 @@ from django.db import models
 
 
 class NoSpamHost(models.Model):
-    host = models.CharField(max_length=200, unique=True)
+    host = models.CharField(max_length=200, unique=True, help_text="sending host not checked for spam")
     
     class Meta:
         ordering = ['host']
-        verbose_name = 'ignored host'
+        verbose_name = 'trusted host'
 
     def __unicode__(self):
         return self.host
 
 
 class NoSpamRecipient(models.Model):
-    address = models.EmailField(max_length=254, unique=True)
+    address = models.EmailField(max_length=254, unique=True, help_text="address not checked for spam")
     
     class Meta:
         ordering = ['address']
-        verbose_name = 'ignored address'
-        verbose_name_plural = 'ignored addresses'
+        verbose_name = 'whitelist address'
+        verbose_name_plural = 'whitelist addresses'
 
     def __unicode__(self):
         return self.address
 
 
 class Subject(models.Model):
-    subject = models.CharField(max_length=100, unique=True)
+    subject = models.CharField(max_length=100, unique=True, help_text="considered spam")
     
     class Meta:
         ordering = ['subject']
