3ca1d94228ed6dff350555afc402fb1da6cd1487
Jiri Barton <jbar@hosting4u.cz>
2013-12-17 01:15:15 +0100

    setup

--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
-try:
-    import paver.tasks
-except ImportError:
-    import os
-    if os.path.exists("paver-minilib.zip"):
-        import sys
-        sys.path.insert(0, "paver-minilib.zip")
-    import paver.tasks
+import os.path
+from setuptools import setup, find_packages
 
-paver.tasks.main()
+VERSION = (0, 1)
+__version__ = VERSION
+__versionstr__ = '.'.join(map(str, VERSION))
+
+ROOT = os.path.dirname(__file__)
+requirements = [line.strip() for line in open(os.path.join(ROOT, 'requirements.txt'))]
+
+setup(
+    name = 'telemail',
+    version = __versionstr__,
+    description = 'A simple interface to managing email for a hosting.',
+    long_description = '\n'.join((
+        'telemail',
+        '',
+        'A simple interface to managing email for a hosting.',
+    )),
+    author = 'Jiri Barton',
+    author_email='jbar@tele3.cz',
+    license = 'BSD',
+
+    packages = find_packages(),
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: End Users/Desktop',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    install_requires = [
+        'setuptools>=0.6b1',
+    ] + requirements,
+)
+
