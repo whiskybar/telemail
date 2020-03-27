e2f720c86b53f1e16106a17d66e0fb9630f24a93
Jiri Barton <jbar@tele3.cz>
2020-03-27 19:08:50 +0100

    password reset

--- telemail/project/urls.py
+++ telemail/project/urls.py
@@ -7,6 +7,9 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'telemail.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^admin/password_reset/$', 'django.contrib.auth.views.password_reset', name='admin_password_reset'),
+    url(r'^admin/password_reset/done/$', 'django.contrib.auth.views.password_reset_done', name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/$', 'django.contrib.auth.views.password_reset_confirm', name='password_reset_confirm'),
+    url(r'^reset/done/$', 'django.contrib.auth.views.password_reset_complete', name='password_reset_complete'),
     url(r'^admin/', include(admin.site.urls)),
 )
